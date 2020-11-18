66a66c1cae8afcf8c4671b54e48d36f4a66bd806
Max Tiutmanov <tyutmanovmd@gmail.com>
2020-11-18 22:32:52 +0300

    more tests for balance calculation

--- be/Tests/Calca.Domain.UnitTests/LedgerTests.cs
+++ be/Tests/Calca.Domain.UnitTests/LedgerTests.cs
@@ -25,20 +25,21 @@ namespace Calca.Domain.UnitTests
         }
 
         [Theory]
-        [MemberData("GetSimpleBalanceTestCases")]
-        public void AddOperation_CalcBalance(List<Operation> operations, List<BalanceItem> expectedBalanceItems)
+        [MemberData(nameof(GetSimpleBalanceTestCases))]
+        public void AddOperations_CalculateBalance(string testCaseName, List<Operation> operations, List<BalanceItem> expectedBalanceItems)
         {
             var sut = CreateSut();
             operations.ForEach(op => sut.AddOperation(op));
 
             var report = sut.GetBalanceReport();
-            report.Items.Should().BeEquivalentTo(expectedBalanceItems);
+            report.Items.Should().BeEquivalentTo(expectedBalanceItems, because: testCaseName);
         }
 
         public static IEnumerable<object[]> GetSimpleBalanceTestCases()
         {
             yield return new object[]
             {
+                "single members pay for other single members",
                 new List<Operation>()
                 {
                     new Operation(DateTime.UtcNow, new[] { _m1 }, new[] { _m2 }, 10m),
@@ -55,17 +56,50 @@ namespace Calca.Domain.UnitTests
 
             yield return new object[]
             {
+                "pair of members pay for another pair of members",
                 new List<Operation>()
                 {
                     new Operation(DateTime.UtcNow, new[] { _m1, _m2 }, new[] { _m3, _m4 }, 10m),
-                    new Operation(DateTime.UtcNow, new[] { _m3, _m4 }, new[] { _m1, _m2 }, 10m)
+                    new Operation(DateTime.UtcNow, new[] { _m1, _m2 }, new[] { _m3, _m4 }, 10m),
                 },
                 new List<BalanceItem>()
                 {
-                    new BalanceItem(_m1, 0m),
-                    new BalanceItem(_m2, 0m),
-                    new BalanceItem(_m3, 0m),
-                    new BalanceItem(_m4, 0m)
+                    new BalanceItem(_m1, 10m),
+                    new BalanceItem(_m2, 10m),
+                    new BalanceItem(_m3, -10m),
+                    new BalanceItem(_m4, -10m)
+                }
+            };
+
+            yield return new object[]
+            {
+                "pair of members pays for everyone",
+                new List<Operation>()
+                {
+                    new Operation(DateTime.UtcNow, new[] { _m1, _m2 }, new[] { _m1, _m2, _m3, _m4 }, 10m),
+                },
+                new List<BalanceItem>()
+                {
+                    new BalanceItem(_m1, 2.5m),
+                    new BalanceItem(_m2, 2.5m),
+                    new BalanceItem(_m3, -2.5m),
+                    new BalanceItem(_m4, -2.5m)
+                }
+            };
+
+            yield return new object[]
+            {
+                "all other members pay for someone",
+                new List<Operation>()
+                {
+                    new Operation(DateTime.UtcNow, new[] { _m1, _m2, _m3 }, new[] { _m4 }, 15m),
+                },
+                new List<BalanceItem>()
+                {
+                    new BalanceItem(_m1, 5m),
+                    new BalanceItem(_m2, 5m),
+                    new BalanceItem(_m3, 5m),
+                    new BalanceItem(_m4, -15m)
                 }
             };
         }
